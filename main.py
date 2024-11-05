b337bb265848e33ff7b3d831d29e895c53664811
Nastya <polishuk.anastasiya2003@gmail.com>
2024-11-05 18:58:46 +0700

    homefile

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
-
+number_1: int = 12
+number_2 = 16
+print(number_1,
+      number_2,
+      )
+string_1 = 'hello "world"'
+string_2 = "hello 'world'"
+string_3 = "hello \"world\""
+string_4 = "\n"
+print(string_1,
+      string_4,
+      string_2,
+      string_4,
+      string_3
+      )
